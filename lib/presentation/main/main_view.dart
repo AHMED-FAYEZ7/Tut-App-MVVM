162a36336a86c60368a31c3151dd363490e4f9bd
faz <ahmedfayez834@gmail.com>
2022-08-26 19:37:13 +0200

    main flow

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -1,3 +1,10 @@
+import 'package:advance_course_flutter/presentation/main/home_page.dart';
+import 'package:advance_course_flutter/presentation/main/notification_page.dart';
+import 'package:advance_course_flutter/presentation/main/search_page.dart';
+import 'package:advance_course_flutter/presentation/main/settings_page.dart';
+import 'package:advance_course_flutter/presentation/resources/color_manger.dart';
+import 'package:advance_course_flutter/presentation/resources/strings_manger.dart';
+import 'package:advance_course_flutter/presentation/resources/values_manger.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatefulWidget {
@@ -8,8 +15,73 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+
+  List<Widget> pages =[
+    HomePage(),
+    SearchPage(),
+    NotificationPage(),
+    SettingsPage(),
+  ];
+
+  List<String> titles =[
+    AppStrings.home,
+    AppStrings.search,
+    AppStrings.notification,
+    AppStrings.settings,
+  ];
+
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          titles[_currentIndex],
+          style: Theme.of(context).textTheme.headline2,
+        ),
+      ),
+      body: pages[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: ColorManger.lightGrey,
+              spreadRadius: AppSize.s1,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          selectedItemColor: ColorManger.primary,
+          unselectedItemColor: ColorManger.grey,
+          currentIndex: _currentIndex,
+          onTap: onTap,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: AppStrings.home,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: AppStrings.search,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: AppStrings.notification,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: AppStrings.settings,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  onTap(int index){
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
